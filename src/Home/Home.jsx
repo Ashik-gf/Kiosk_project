54e65bec6d21bbf6dea874392ceea359d966fe18
ashik-coder <ashikhasan998@gmail.com>
2024-12-10 14:46:00 +0600

    uplode this

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div className=' flex justify-center items-center py-12'>
-        <Link to={'/inputFrom'}>
-         <button className=' border-2  px-3 py-1 rounded-md bg-gradient-to-l from-blue-500 text-White'>
-            Uplode Content
+    <div className=' bg-blue-500 border-2 border-red-600 h-[1000px] w-full flex justify-center items-center py-12'>
+      <Link to={'/inputFrom'}>
+        <button className=' border-2  px-3 py-1 rounded-md bg-gradient-to-l from-blue-500 text-White'>
+          Uplode Content
         </button>
-        </Link>
-        <Link to={'/playContent'}>
-         <button className=' border-2  px-3 py-1 rounded-md bg-gradient-to-l from-blue-500 text-White'>
-            Play
+      </Link>
+      <Link to={'/playContent'}>
+        <button className=' border-2  px-3 py-1 rounded-md bg-gradient-to-l from-blue-500 text-White'>
+          Play
         </button>
-        </Link>
-       
+      </Link>
+
     </div>
   )
 }
